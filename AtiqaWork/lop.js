ce2cfecae7e8449e98c84afa68ea5910c6c737e2
atiqatariq007 <atiqatariq007@outlook.com>
2025-09-09 22:23:06 +0500

    i used it for rough work for code clarity

---
+++ AtiqaWork/lop.js
@@ -0,0 +1,26 @@
+
+
+// let numDivThree = 1
+
+// for(i =1 ; i<51 ; i++){
+//    if (numDivThree%3 == 0){
+//     console.log(numDivThree);
+//    }
+//    numDivThree++
+//    };
+
+// let p=1;
+// let count = 0;
+// for(i=1; i<=100;i++){
+//     if(p%5 == 0){
+//     count=count+1;
+//     }
+//     p++;
+// }
+// console.log(count);
+
+
+for(i=10; i>=0; i--){
+     console.log(i);
+    };
+    console.log("Blast off");
