29bccd4ddd9790db50fed2a7a9c67e8779cea925
atiqatariq007 <atiqatariq007@outlook.com>
2025-09-09 22:24:21 +0500

    rough work for my first application

---
+++ ProjectWork/Atiqa/Module5/roughwork.js
@@ -0,0 +1,66 @@
+import createPrompt from 'prompt-sync';
+let prompt = createPrompt();
+// let school =  [];
+
+// let student = {};
+// let firstaname = "atiqa";
+// let lastName = "Tariq"
+// let condition = "active"
+// student.firstaname = firstaname;
+// student.lastName = lastName ;
+// student.condition = condition ;
+
+// school.push(student);
+
+// student = {};
+// firstaname = "hira";
+//  lastName = "Tariq"
+//   condition = "not active"
+
+
+// student.firstaname = firstaname;
+// student.lastName = lastName ;
+// student.condition = condition ;
+
+
+// school.push(student);
+// console.log (school);
+
+// student = {};
+// firstaname = "ayesha";
+//  lastName = "farakh"
+//   condition = "not active"
+
+
+// student.firstaname = firstaname;
+// student.lastName = lastName ;
+// student.condition = condition ;
+
+
+// school.push(student);
+// //console.log (school);
+
+
+// for (let index =0 ; index < school.length; index++);
+// console.log(school[index]);
+while(true){
+  let menu= prompt ("menu : ")
+if (menu ===("1")){
+  console.log ("a")
+}
+else if (menu === ("2")){
+  console.log("b")
+}
+else if (menu === ("3")){
+  console.log("c")
+}
+else if (menu === ("4")){
+  console.log("d")
+}
+else if (menu === ("5")){
+  console.log("e")
+}
+else{
+  console.log("incorrect entry")
+}
+}
